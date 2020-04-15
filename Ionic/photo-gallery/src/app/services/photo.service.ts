634443b32eae04bef675d400c42f79439f8e5914
Thomas Sihapanya <sihapanya.thomas@gmail.com>
2020-04-15 11:15:28 +0200

    Ionic end

--- Ionic/photo-gallery/src/app/services/photo.service.ts
+++ Ionic/photo-gallery/src/app/services/photo.service.ts
@@ -13,6 +13,7 @@ const { Camera, Filesystem, Storage } = Plugins;
 export class PhotoService {
 
   public photos: Photo[] = [];
+  private PHOTO_STORAGE: string = "photos";
 
   constructor() { }
 
@@ -24,9 +25,84 @@ export class PhotoService {
       quality: 100
     });
 
-    this.photos.unshift({
-      filepath: "soon...",
-      webviewPath: capturedPhoto.webPath
+    // 1. On sauvegarde la photo avec notre nouvelle méthode savePicture()
+    const savedImageFile = await this.savePicture(capturedPhoto);
+
+    // 2.  On ajoute notre image sauvegardée à notre tableau de photos
+    this.photos.unshift(savedImageFile);
+
+    Storage.set({
+      key: this.PHOTO_STORAGE,
+      value: JSON.stringify(this.photos.map(p => {
+        // Don't save the base64 representation of the photo data, 
+        // since it's already saved on the Filesystem
+        const photoCopy = { ...p };
+        delete photoCopy.base64;
+
+        return photoCopy;
+      }))
     });
+  }
+
+  private async savePicture(cameraPhoto: CameraPhoto) {
+    // Convert photo to base64 format, required by Filesystem API to save
+    const base64Data = await this.readAsBase64(cameraPhoto);
+
+    // Write the file to the data directory
+    const fileName = new Date().getTime() + '.jpeg';
+    console.log(fileName);
+    await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: FilesystemDirectory.Data
+    });
+
+    console.log(FilesystemDirectory.Data);
+
+    // Get platform-specific photo filepaths
+    return await this.getPhotoFile(cameraPhoto, fileName);
+  }
+
+  private async readAsBase64(cameraPhoto: CameraPhoto) {
+    // Fetch the photo, read as a blob, then convert to base64 format
+    const response = await fetch(cameraPhoto.webPath!);
+    const blob = await response.blob();
+
+    return await this.convertBlobToBase64(blob) as string;
+  }
+
+  convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader;
+    reader.onerror = reject;
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
+
+  private async getPhotoFile(cameraPhoto: CameraPhoto,
+    fileName: string): Promise<Photo> {
+    return {
+      filepath: fileName,
+      webviewPath: cameraPhoto.webPath
+    };
+  }
+
+  public async loadSaved() {
+    // Retrieve cached photo array data
+    const photos = await Storage.get({ key: this.PHOTO_STORAGE });
+    this.photos = JSON.parse(photos.value) || [];
+
+    // Display the photo by reading into base64 format
+    for (let photo of this.photos) {
+      // Read each saved photo's data from the Filesystem
+      const readFile = await Filesystem.readFile({
+        path: photo.filepath,
+        directory: FilesystemDirectory.Data
+      });
+
+      // Web platform only: Save the photo into the base64 field
+      photo.base64 = `data:image/jpeg;base64,${readFile.data}`;
+    }
   }
 }
--- Ionic/photo-gallery/src/app/tab2/tab2.page.ts
+++ Ionic/photo-gallery/src/app/tab2/tab2.page.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import  { PhotoService } from '../services/photo.service';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
   constructor(public photoService: PhotoService) {}
+
+
+  ngOnInit() {
+    this.photoService.loadSaved();
+  }
 
 }
